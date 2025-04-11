0d9de4e583bcd9cf87a21574abd43a70b5d837fb
thepaperpilot <thepaperpilot@gmail.com>
2025-04-10 22:01:24 -0500

    Add env var config and fix issues

--- index.ts
+++ index.ts
@@ -1,15 +1,18 @@
+import { type S3ClientConfig } from "@aws-sdk/client-s3";
 import { StorageManager } from "@muni-town/leaf/storage";
 import { SuperPeer1 } from "@muni-town/leaf/sync1";
-import { s3StorageAdapter } from "https://raw.githubusercontent.com/thepaperpilot/LeafS3StorageAdapter/3b33339fc447e43b11cead55c443075e057f43ad/src/index.ts";
-import { addUser, removeUser, handleRequest } from "https://raw.githubusercontent.com/thepaperpilot/AuthenticatedLeafSyncServer/29c23bd972007687e330f64b388d30398900e225/src/index.ts";
+import { s3StorageAdapter } from "s3StorageAdapter";
+import { addUser, handleRequest, removeUser } from "authenticatedSyncServer";
 
 /** Start a websocket sync server */
-export async function startServer(opts: { port: number }) {
-
+export function startServer(
+  opts: { port: number; bucket: string } & S3ClientConfig,
+) {
+  const { port, bucket, ...clientConfig } = opts;
   const superPeer = new SuperPeer1(
-    new StorageManager(s3StorageAdapter(db)),
+    new StorageManager(s3StorageAdapter(bucket, clientConfig)),
   );
-  const server = Deno.serve({ port: opts.port }, (req) => {
+  const server = Deno.serve({ port }, (req) => {
     return handleRequest(superPeer, req);
   });
 
@@ -17,14 +20,27 @@ export async function startServer(opts: { port: number }) {
 }
 
 if (import.meta.main) {
-  const [command, ...args] = Deno.args[0];
+  const [command, ...args] = Deno.args;
   if (command === "add_user" && args.length >= 2) {
     addUser(args[0], args[1]);
   } else if (command === "remove_user" && args.length >= 1) {
     removeUser(args[0]);
   } else {
     const port = parseInt(Deno.env.get("PORT") || "8000");
+    const bucket = Deno.env.get("BUCKET");
+    const accessKeyId = Deno.env.get("ACCESS_KEY_ID");
+    const secretAccessKey = Deno.env.get("SECRET_ACCESS_KEY");
+    const region = Deno.env.get("REGION");
+    const endpoint = Deno.env.get("ENDPOINT");
 
-    startServer({ port });
+    startServer({
+      port,
+      bucket,
+      {
+        credentials: { accessKeyId, secretAccessKey },
+        region,
+        endpoint
+      }
+    });
   }
 }
